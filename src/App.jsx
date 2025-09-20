534f5f75d860ae9bba0adc5f09ba0ad69135869c
rahulpreethamsai <chrahulpreethamsai@gmail.com>
2025-09-20 22:18:05 +0530

    "Deployed Commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/HomePage/Home';
 import AuthForm from './components/auth/AuthForm';
 import AttractionsPage from './components/pages/AttractionsPage/AttractionsPage';
 import AttractionsDetails from './components/pages/AttractionsDetailsPage/AttractionsDetails';
-import MyTicketsPage from './components/pages/MyTicketsPage/MyTicketsPage'; // New Page
-import { AuthProvider } from './context/AuthContext.jsx';
+import MyTicketsPage from './components/pages/MyTicketsPage/MyTicketsPage';
+import AboutUs from './components/pages/AboutUsPage/AboutPage';
 
 function App() {
+  // No <Router> here! It's now in main.jsx
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<AuthForm />} />
-          <Route path="/events" element={<AttractionsPage />} />
-          <Route path="/events/:id" element={<AttractionsDetails />} />
-          <Route path="/mytickets" element={<MyTicketsPage />} /> {/* New Route */}
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<AuthForm />} />
+      <Route path="/events" element={<AttractionsPage />} />
+      <Route path="/events/:id" element={<AttractionsDetails />} />
+      <Route path="/mytickets" element={<MyTicketsPage />} />
+      <Route path="/about" element={<AboutUs />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import './index.css'; // Assuming you have a global CSS file
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    {/* The Router should be here, at the highest level */}
+    <BrowserRouter>
+      {/* The AuthProvider provides user context to the whole app */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
